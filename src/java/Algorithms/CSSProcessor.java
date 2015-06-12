1d5dba9c53ec2ab0edaaee6107bc4ae4b18b34f5
jim-smith <james.smith@uwe.ac.uk>
2015-06-12 12:19:02 +0100

    updating cssprocessor to kieran;s version

--- src/java/Algorithms/CSSProcessor.java
+++ src/java/Algorithms/CSSProcessor.java
@@ -13,7 +13,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.Vector;
 
 /**
@@ -22,27 +24,27 @@ import java.util.Vector;
  */
 public class CSSProcessor implements Processor {
 
-    private Hashtable cssLabels;
-  
-    
+    private HashMap cssLabels;
+
     public CSSProcessor() {
-       this.cssLabels = setupCSSLabelStore();
-      
+        this.cssLabels = setupCSSLabelStore();
+
     }
 
     /*
-    Profile - the profile being applied to artifact
-    Artifact - the raw artifact to be processed
-    index - the identifier for the profile
-    */
+     Profile - the profile being applied to artifact
+     Artifact - the raw artifact to be processed
+     index - the identifier for the profile
+     */
     @Override
     public Artifact applyProfileToArtifact(Profile profile, Artifact artifact, String outputFolder) {
 
-        
-        Hashtable kernels = profile.getKernels();
-        if (kernels == null) {System.out.println("Error: applyProfileToArtifcat in CSSProcessor. No kernels present in Profile.");}
+        HashMap kernels = profile.getKernels();
+        if (kernels == null) {
+            System.out.println("Error: applyProfileToArtifcat in CSSProcessor. No kernels present in Profile.");
+        }
 
-	// ----------- CSS Formatters -------------------------------//
+        // ----------- CSS Formatters -------------------------------//
         String CSS_Start_Braces = "{";
         String CSS_End_Braces = "}";
         String CSS_PropSeparator = ":";
@@ -50,16 +52,19 @@ public class CSSProcessor implements Processor {
 
         // ------------ CSS Generation ------------------------------//
         String css = "";
-        Hashtable pv = profile.getSolutionAttributes();
-        if (pv == null) { System.out.println("Error: applyProfileToArtifcat in CSSProcessor. No solution attributes in Profile.");}
-        Enumeration pvarsEnu = pv.keys();
+        HashMap pv = profile.getSolutionAttributes();
+        if (pv == null) {
+            System.out.println("Error: applyProfileToArtifcat in CSSProcessor. No solution attributes in Profile.");
+        }
+        Set keySet = pv.keySet();
+        Iterator iterator = keySet.iterator();
         String csspLine = "body{";
         int colorCheck = 0;
         int red = 0;
         int blue = 0;
         int green = 0;
-        while (pvarsEnu.hasMoreElements()) {
-            String vkey = pvarsEnu.nextElement().toString();
+        while (iterator.hasNext()) {
+            String vkey = iterator.next().toString();
             SolutionAttributes ipvar = (SolutionAttributes) pv.get(vkey);
 
             if (ipvar.getName().contains("Page")) {
@@ -84,11 +89,12 @@ public class CSSProcessor implements Processor {
 
         csspLine += CSS_End_Braces + "\n";
         css += csspLine;
-        Enumeration kernelsEnuTemp = kernels.keys();
+        Set keySet1 = kernels.keySet();
+        Iterator kernelsEnuTemp = keySet1.iterator();
         String[] tempVector = new String[kernels.size()];
         int k = 3;
-        while (kernelsEnuTemp.hasMoreElements()) {
-            String kernelName = kernelsEnuTemp.nextElement().toString();
+        while (kernelsEnuTemp.hasNext()) {
+            String kernelName = kernelsEnuTemp.next().toString();
             if (kernelName.equalsIgnoreCase("h1")) {
                 tempVector[0] = kernelName;
             } else if (kernelName.equalsIgnoreCase("h2")) {
@@ -112,14 +118,16 @@ public class CSSProcessor implements Processor {
             String ktype = kernelsEnu.nextElement().toString();
             Kernel kernel1 = (Kernel) kernels.get(ktype);
             cssLine += kernel1.getName() + CSS_Start_Braces;
-            Hashtable vars = kernel1.getVariables();
-            Enumeration evars = vars.keys();
+            HashMap vars = kernel1.getVariables();
+            Set keySet2 = vars.keySet();
+
+            Iterator evars = keySet2.iterator();
             colorCheck = 0;
             red = 0;
             blue = 0;
             green = 0;
-            while (evars.hasMoreElements()) {
-                String vkey = evars.nextElement().toString();
+            while (evars.hasNext()) {
+                String vkey = evars.next().toString();
                 SolutionAttributes ipvar = (SolutionAttributes) vars.get(vkey);
 
                 if (ipvar.getName().contains("color")) {
@@ -146,14 +154,14 @@ public class CSSProcessor implements Processor {
                         String value = (String) values.get(val.intValue());
                         cssLine += ipvar.getName() + CSS_PropSeparator + value
                                 + CSS_PropPairSeparator;
-                        
+
                     } else if (ipvar.getType().equalsIgnoreCase("ordinal")) {
                         Double val = ipvar.getValue();
-                        
+
                         if (ktype.equalsIgnoreCase("h1")) {
                             val = CSS_lastfontsize * ((val / 100));
                             CSS_lastfontsize = val;
-                           
+
                         } else if (ktype.equalsIgnoreCase("h2") || ktype.equalsIgnoreCase("p")) {
                             val = CSS_lastfontsize * 0.5 * (1.0 + (val) / 100);
                             CSS_lastfontsize = val;
@@ -174,7 +182,6 @@ public class CSSProcessor implements Processor {
         String CSS = css;
 
         // ---------- Filenames Generation------------//
-    
         String outHtmlPath;
         String processedArtifactName;
         String profileName = profile.getName();
@@ -183,29 +190,29 @@ public class CSSProcessor implements Processor {
 
         try {
             String rawArtifactName = artifact.getFilename();
-             rawArtifactName = rawArtifactName.substring(0, rawArtifactName.lastIndexOf('.'));
+            rawArtifactName = rawArtifactName.substring(0, rawArtifactName.lastIndexOf('.'));
              // TESTING : distinguishing the raw artifact name from the processed one (processed one)
-           //  System.out.println("Raw artifact name = " + rawArtifactName + " : profilename = " + profileName);
-             processedArtifactName = profileName + "-" +rawArtifactName + ".html";
-             System.out.println("Processed artifact name = " + processedArtifactName);
-                outHtmlPath = outputFolder + processedArtifactName;
-                String htmlFile = "";
-                BufferedReader reader = new BufferedReader(new FileReader(artifact.getFile().getAbsolutePath()));
-                String temp;
-                while ((temp = reader.readLine()) != null) {
-                    htmlFile += temp + "\n";
-                    if (temp.contains("<head>")) {
-                        htmlFile += "<style type=\"text/css\">";
-                        htmlFile += CSS;
-                        htmlFile += "</style>";
-                    }
+            //  System.out.println("Raw artifact name = " + rawArtifactName + " : profilename = " + profileName);
+            processedArtifactName = profileName + "-" + rawArtifactName + ".html";
+            System.out.println("Processed artifact name = " + processedArtifactName);
+            outHtmlPath = outputFolder + processedArtifactName;
+            String htmlFile = "";
+            BufferedReader reader = new BufferedReader(new FileReader(artifact.getFile().getAbsolutePath()));
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                htmlFile += temp + "\n";
+                if (temp.contains("<head>")) {
+                    htmlFile += "<style type=\"text/css\">";
+                    htmlFile += CSS;
+                    htmlFile += "</style>";
                 }
+            }
 
-               BufferedWriter  writer = new BufferedWriter(new FileWriter(outHtmlPath));
-                writer.write(htmlFile);
-                writer.close();
-              
-                return new Artifact(new File(outHtmlPath));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outHtmlPath));
+            writer.write(htmlFile);
+            writer.close();
+
+            return new Artifact(new File(outHtmlPath));
         } catch (Exception e) {
             System.out.println("");
             e.printStackTrace();
@@ -213,7 +220,7 @@ public class CSSProcessor implements Processor {
         return null;
     }
 
-     public Hashtable setupCSSLabelStore() {
+    public HashMap setupCSSLabelStore() {
         // Cardinal variables store
 
         // The fontfamilies.
@@ -231,7 +238,7 @@ public class CSSProcessor implements Processor {
                 = {"0px 0px 10px 10px", "10px 10px 0px 0px", "0px 10px 10px 0px",
                     "10px 0px 0px 10px", "10px 0px 10px 0px"};
 
-        Hashtable cssStore = new Hashtable();
+        HashMap cssStore = new HashMap();
 
         Vector temp = new Vector();
         for (int i = 0; i < fontfamilies.length; i++) {
@@ -253,5 +260,5 @@ public class CSSProcessor implements Processor {
 
         return cssStore;
     }
-    
+
 }
