0b1246b630bd6cae159f3891a295c724116a46ab
kieran <kieran@5.151.116.21>
2015-07-03 14:28:50 +0100

    changed project name to allow the logging file to be placed into the correct folder on the server (/IPAT/logs/log4j-IPAT.log)

--- src/java/Src/Controller.java
+++ src/java/Src/Controller.java
@@ -168,7 +168,8 @@ public class Controller {
         File[] profiles_list = profileFolder.listFiles(filter);
         if (profiles_list == null) {
             logger.error("Error : profiles_list  == null in bootstrap application. Please check the web.xml in WEB-INF to ensure paths to config folders are correct.\n");
-           logger.error("profile folder with path : " + profileFolder.getAbsolutePath() + " with "+profileFolder.listFiles().length+" files : \n");
+           logger.error("profile folder with path : " + profileFolder.getAbsolutePath());
+           logger.error(" with "+profileFolder.listFiles().length+" files : \n");
             for (File profile : profileFolder.listFiles()) {
                 logger.error(profile + "\n");
             }
